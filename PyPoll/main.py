c22a19e3c3c227a7b5377ab707e5ee7b923433b7
BryantZuki <Bryantsmith91@gmail.com>
2019-03-23 20:54:32 -0400

    Counts the votes.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,9 +1,43 @@
 import os
 import csv
 
-csvpath = os.path.join('..','PyPoll','election_data.csv')
+# Correy
+# Khan
+# Li
+# O'tooley
+ccount = 0
+kcount = 0
+lcount = 0
+ocount = 0
+
+clist = []
+
+# csvpath = os.path.join('..','PyPoll','election_data.csv')
+csvpath = 'C:/Users/bryan/Desktop/BootCampStuff/Homework/HW_03_Python/PyPoll/election_data.csv'
+
 with open(csvpath, newline='') as csvfile:
-        csvreader = csv.reader(csvfile, delimiter =',')
+    csvreader = csv.reader(csvfile, delimiter =',')
+    header = next(csvreader)
+    for row in csvreader:
+        clist.append(row[2])
+ccount = 0
+kcount = 0
+lcount = 0
+ocount = 0
 
+for i in range(len(clist)):
 
-        print(len(csvreader))
+    if clist[i] == "Khan":
+        kcount = kcount + 1
+    elif clist[i] == "Correy":
+        ccount = ccount + 1
+    elif clist[i] == "Li":
+        lcount = lcount + 1
+    elif clist[i] == "O'Tooley":
+        ocount = ocount + 1
+
+print (kcount, ccount, lcount, ocount)
+        # print(str(kcount))
+        # for row in csvreader:
+        #     if  is 'Khan':
+        #         kcount = kcount + 1
