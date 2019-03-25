98e076d54fa9a1d9eed4bd3da750e6e5aed9d6b4
BryantZuki <Bryantsmith91@gmail.com>
2019-03-24 21:52:23 -0400

    Goodnight - find a way to make difference work please.

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,46 +4,59 @@ import csv
 # importing counter module
 from collections import Counter
 
+def difference(a):
+    return round((a + 1) - a, 2)
+
 #setting csv path
 # csvpath = os.path.join('..','PyBank','budget_data.csv')
 csvpath = "C:/Users/bryan/Desktop/BootCampStuff/Homework/HW_03_Python/PyBank/budget_data.csv"
 
 #setting list for counter
 clist = []
-clist2 = []
+maxdif = 0
+mindif = 0
 total = 0
 # opens CSV file, sets delimiter and appends results to a list
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter =',')
     next(csvreader, None)
-    for i in csvreader:
-        total = total + i[1]
     for row in csvreader:
         clist.append(row[1])
+        total = total + int(row[1])
+
+
+        # Broken here or the function is broken idk.
+
+        ########## if difference(int(row[1])) > maxdif:
+        ##########     maxdif = difference(int(row[1]))
+        ##########     maxdifmo = row[0]
+        ##########     diflist.append(difference(row[1]))
+        ########## elif difference(int(row[1])) < mindif:
+        ##########     mindif = difference(int(row[1]))
+        ##########     mindifmo = row[0]
+        ##########     diflist.append(difference(row[1]))
+        ########## else:
+        ##########     diflist.append(difference(row[1]))
 
 
 totalmoney = Counter(clist)
-# maximum = max(totalmoney, key=lambda key: totalmoney[key])
-# minimum = min(totalmoney, key=lambda key: totalmoney[key])
-print(maximum, minimum)
-# for i in totalmoney:
-#     clist2.append(int(i))
-#
-# total = sum(totalmoney.elements())
-
 totalmonths = len(clist)
 
-print(f'{total},{totalmonths}')
-
-
+print("Financial Analysis")
+print("-"*28)
+print("Total Months: "+str(totalmonths))
+print("Total: $"+str(total))
+# print("Average Change: $"+str())
+# print("Greatest Increase in Profits: "+str()+" ($"+str()+")")
+# print("Greatest Decrease in Profits: "+str()+" ($"+str()+")")
 
 # #prints end to textfile
 # with open("pybank.txt", "w") as outfile:
 #     print("Financial Analysis", file=outfile)
 #     print("-"*28, file=outfile)
-#     print("Total Months: "+str(), file=outfile)
-#     print("Total: $"+str(), file=outfile)
-#     print("Average Change: $"+str(), file=outfile)
-#     print("Greatest Increase in Profits: "+str()+" ($"+str()+")", file=outfile)
-#     print("Greatest Decrease in Profits: "+str()+" ($"+str()+")", file=outfile)
+#     print("Total Months: "+str(totalmonths), file=outfile)
+#     print("Total: $"+str(total), file=outfile)
+#     print("Average Change: $"+str(##############), file=outfile)
+#     print("Greatest Increase in Profits: "+str(########)+" ($"+str(########)+")", file=outfile)
+#     print("Greatest Decrease in Profits: "+str(#######)+" ($"+str(########)+")", file=outfile)
 # outfile.close()
