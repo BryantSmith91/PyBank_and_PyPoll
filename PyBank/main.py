d33ce63f9936429cf87eefca3eb7206d1f174337
BryantZuki <Bryantsmith91@gmail.com>
2019-03-25 20:24:03 -0400

    it works! still need to comment and add textfiles

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,7 +5,7 @@ import csv
 from collections import Counter
 
 def difference(a, b):
-    return round(float(a) - float(b), 2)
+    return round(int(a) - int(b), 2)
 
 #setting csv path
 # csvpath = os.path.join('..','PyBank','budget_data.csv')
@@ -27,47 +27,43 @@ with open(csvpath, newline='') as csvfile:
         v2 = row[1]
         clist.append(row[1])
         total += int(row[1])
-        if int(v2)-int(v1) > int(maxdif):
-            maxdif = int(v2)
+        diflist.append(difference(v2,v1))
+        if difference(v2,v1) > float(maxdif):
+            maxdif = difference(v2,v1)
             maxdifmo = str(row[0])
-            diflist.append(difference(v2,v1))
+            # diflist.append(difference(v2,v1))
             v1 = row[1]
-        # elif mindif > difference(v2,v1):
-        #     mindif = int(v2)
-        #     mindifmo = str(row[0])
-        #     diflist.append(difference(v2,v1))
-        #     v1 = row[1]
+        elif difference(v2,v1) < float(mindif):
+            mindif = difference(v2,v1)
+            mindifmo = str(row[0])
+            # diflist.append(difference(v2,v1))
+            v1 = row[1]
         else:
             v1 = row[1]
-            diflist.append(difference(v2,v1))
 
-
-
-
-print(maxdif,maxdifmo)
-print(avgdif)
-# print(mindif,mindifmo)
-
-# totalmoney = Counter(clist)
+del diflist[0]
 totalmonths = len(clist)
-
 totaldif = sum(diflist)
-avgdif = int(totaldif/totalmonths)
-# print("Financial Analysis")
-# print("-"*28)
-# print("Total Months: "+str(totalmonths))
-# print("Total: $"+str(total))
-# print("Average Change: $"+str())
-# print("Greatest Increase in Profits: "+str()+" ($"+str()+")")
-# print("Greatest Decrease in Profits: "+str()+" ($"+str()+")")
+avgdif = round(float(totaldif/(totalmonths-1)),2)
+# print(totalmonths,totaldif,avgdif)
 
-# #prints end to textfile
-# with open("pybank.txt", "w") as outfile:
-#     print("Financial Analysis", file=outfile)
-#     print("-"*28, file=outfile)
-#     print("Total Months: "+str(totalmonths), file=outfile)
-#     print("Total: $"+str(total), file=outfile)
-#     print("Average Change: $"+str(##############), file=outfile)
-#     print("Greatest Increase in Profits: "+str(########)+" ($"+str(########)+")", file=outfile)
-#     print("Greatest Decrease in Profits: "+str(#######)+" ($"+str(########)+")", file=outfile)
-# outfile.close()
+
+
+print("Financial Analysis")
+print("-"*28)
+print("Total Months: "+str(totalmonths))
+print("Total: $"+str(total))
+print("Average Change: $"+str(avgdif))
+print("Greatest Increase in Profits: "+str(maxdifmo)+" ($"+str(maxdif)+")")
+print("Greatest Decrease in Profits: "+str(mindifmo)+" ($"+str(mindif)+")")
+
+#prints end to textfile
+with open("pybank.txt", "w") as outfile:
+    print("Financial Analysis", file=outfile)
+    print("-"*28, file=outfile)
+    print("Total Months: "+str(totalmonths), file=outfile)
+    print("Total: $"+str(total), file=outfile)
+    print("Average Change: $"+str(avgdif), file=outfile)
+    print("Greatest Increase in Profits: "+str(maxdifmo)+" ($"+str(maxdif)+")", file=outfile)
+    print("Greatest Decrease in Profits: "+str(mindifmo)+" ($"+str(mindif)+")", file=outfile)
+outfile.close()
